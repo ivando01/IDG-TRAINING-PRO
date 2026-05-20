e6aa5f8f5ae0c8881ee1d05fa004663e965ef523
Ivan Dominguez <ivando01@gmail.com>
2026-05-19 20:50:14 -0500

    Increase activity payload limit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors({
     return callback(new Error("Origen no permitido por CORS"));
   },
 }));
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
 
 /* ================= CONFIG ================= */
 
